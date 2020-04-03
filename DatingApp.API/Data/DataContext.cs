ae81b21cfcc51ffef84ff1a259e2780bbe195c17
Janus Ringling <janusringling2@gmail.com>
2020-04-03 14:24:56 +0200

    init commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options){}
+
+        internal DbSet<Value> Values { get; set; }
+    }
+}
