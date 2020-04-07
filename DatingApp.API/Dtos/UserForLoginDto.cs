7151772eda8abd79e87836f026b04fde2f19269f
Janus Ringling <janusringling2@gmail.com>
2020-04-07 14:13:04 +0200

    a commit

---
+++ DatingApp.API/Dtos/UserForLoginDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatingApp.API.Dtos
+{
+    public class UserForLoginDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+
+    }
+}
--- DatingApp.API/Dtos/UserForRegisterDto.cs
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace DatingApp.API.Dtos
 {
     public class UserForRegisterDto
     {
+
+        [Required]
         public string Username { get; set; }
+
+        [Required]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "Pls make right password")]
         public string Password { get; set; }
     }
 }
